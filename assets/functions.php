4df6ecdf4a18095093c6b2f069a567066b3c3725
Charlie <ctbushman@gmail.com>
2019-11-14 09:06:30 -0500

    Added webpage files
    Added everything in htdoc, db schema to come in another commit

---
+++ assets/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function connect_db() {
+    $servername = "sql303.epizy.com";
+    $database = "epiz_24783286_ctbus_gop";
+    $username = "epiz_24783286";
+    $password = "Pv4KJLgmURsD";
+
+    // Create connection
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    // Check connection
+
+    if ($conn->connect_error || $conn == 0) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+?>
